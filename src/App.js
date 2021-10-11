d50f5ddb6aa5527236c5835b71828fd6a212fd6a
Greg <gregoire.ansard@gmail.com>
2021-10-11 17:55:00 +0200

    ajouter les images

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Images from './Images.js'
+
+function App() {
+  return (
+    <div className="App">
+      {Images.Baki}
+      {Images.Eren}
+      {Images.Itachi}
+      {Images.Kirua}
+      {Images.Ligth}
+      {Images.Livai}
+      {Images.Luffy}
+      {Images.Mikassa}
+      {Images.Minato}
+      {Images.Naruto}
+      {Images.Zoro}
+    </div>
+  );
+}
+
+export default App;
